110ed7540151147836149029c7a8be777e7ff998
synchcyberdescron <synchcyberdescron@gmail.com>
2023-05-04 21:44:15 +0300

    Js

---
+++ Javascript/index.js
@@ -0,0 +1,8 @@
+
+let name1 = "Tunç";
+let age = 24;
+let isStudent = false;
+document.getElementById("p1").innerHTML = "Hello " , name1;
+document.getElementById("p2").innerHTML = "You are "+age;
+document.getElementById("p3").innerHTML = "Is student: "+isStudent;
+
