5e9233e49e34dd67a22b7005fd807869ba591d83
synchcyberdescron <synchcyberdescron@gmail.com>
2023-05-05 18:55:09 +0300

    HTML-CSS-JS
    Adding functionality to a website with JS

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,3 +6,30 @@ document.getElementById("p1").innerHTML = "Hello " , name1;
 document.getElementById("p2").innerHTML = "You are "+age;
 document.getElementById("p3").innerHTML = "Is student: "+isStudent;
 
+ // let username = window.prompt("Whats your name?");
+ // console.log(username);
+let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
+let count = 0;
+document.getElementById("increase").onclick = function(){
+    count += 1;
+    document.getElementById("count").innerHTML = count;
+}
+document.getElementById("decrease").onclick = function(){
+    count -= 1;
+    document.getElementById("count").innerHTML = count;
+}
+document.getElementById("reset").onclick = function(){
+    count = 0;
+    document.getElementById("count").innerHTML = count;
+}
+let rollldice = 0
+document.getElementById("roll1").onclick = function(){
+    rollldice =Math.round( Math.random() * 100);
+    document.getElementById("roll").innerHTML = rollldice;
+
+}
+
